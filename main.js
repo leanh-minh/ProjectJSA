3b77db78caf2897500460ed87d97fecbf2de9f15
leanhminh <4nhminh2000@gmail.com>
2023-08-26 20:38:51 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,156 @@
+let data = [
+    [
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2020/08/trangvangsonmaidacbiet-300x300.jpg",
+            "name":"Trăng Vàng BLACK&GOLD Kim Cương 6Bánh 160G + Trà – 2023",
+            "price":"5.000.000 ₫",
+            "type":"Bánh Trung Thu Trăng Vàng Thượng Hạng Black & Gold",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2022/07/kinhdoyen-1-300x300.jpg",
+            "name":"Trăng Vàng BLACK & GOLD Yến Sào 4 bánh 160G + Trà – 2023",
+            "price":"1.900.000 ₫",
+            "type":"Bánh Trung Thu Trăng Vàng Thượng Hạng Black & Gold",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2020/08/trangvanghop4-banh.jpg",
+            "name":"Trăng Vàng BLACK N GOLD Hộp 4 Bánh x160g và Trà – 2023",
+            "price":"1.500.000 ₫",
+            "type":"Bánh Trung Thu Trăng Vàng Thượng Hạng Black & Gold",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2020/08/trangvanghoptron-2-banh-300x300.jpg",
+            "name":"Trăng Vàng BLACK N GOLD Hộp 2 Bánh x160g và Trà – 2023",
+            "price":"640.000 ₫",
+            "type":"Bánh Trung Thu Trăng Vàng Thượng Hạng Black & Gold",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2023/…thu-kinh-do-25-nam-phien-ban-gioi-han-300x300.jpg",
+            "name":"Hộp Bánh Trung Thu Kinh Đô 25 Năm – Phiên Bản Giới Hạn",
+            "price":"680.000 ₫",
+            "type":"Bánh Trung Thu Trăng Vàng Thượng Hạng Black & Gold",
+        },
+    ],
+    [
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2021/08/trang-vang-pha-le-van-phuc-xanh-thumb.jpg",
+            "name":"Trăng Vàng Pha Lê Vạn Phúc 2023",
+            "price":"980.000 ₫",
+            "type":"Bánh Trung Thu Trăng Vàng 2023",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2021/08/7B-1.png",
+            "name":"Trăng Vàng Hồng Ngọc Tâm An 4BX120 2023 – Bánh Dùng Đường Ăn Kiêng",
+            "price":"570.000 ₫",
+            "type":"Bánh Trung Thu Trăng Vàng 2023",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2022/07/Trangvanghongngochungphu2.jpg",
+            "name":"Trăng Vàng Hồng Ngọc Hưng Phú 8 Bánh 80G – 2023",
+            "price":"770.000 ₫",
+            "type":"Bánh Trung Thu Trăng Vàng 2023",
+        },
+        {
+            "img":"	https://banhkinhdo.com.vn/wp-content/uploads/2021/08/5A.png",
+            "name":"Trăng Vàng Hồng Ngọc An Thịnh (4Bx160G)2023",
+            "price":"770.000 ₫",
+            "type":"Bánh Trung Thu Trăng Vàng 2023",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2020/08/trang-vang-hong-ngoc-an-phu-vang.png",
+            "name":"Trăng Vàng Hồng Ngọc An Phú (4Bx160G) 2023",
+            "price":"770.000 ₫",
+            "type":"Bánh Trung Thu Trăng Vàng 2023",
+        },
+    ],
+    [
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2022/08/KIDOpresident-2048x1225.jpg",
+            "name":"Hộp Bánh Trung Thu KIDO – Hộp PRESIDENT 6Bx180GR và Trà Shan Tuyết 2023",
+            "price":"3.000.000 ₫",
+            "type":"Bánh Trung Thu KiDo’s Bakery",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2022/08/KIDOthuthinhvuong-do-2048x1883.jpg",
+            "name":"Hộp Bánh Trung Thu KIDO THU THỊNH VƯỢNG ĐỎ – Hộp Quà Biếu Cao Cấp 2023",
+            "price":"410.000 ₫",
+            "type":"Bánh Trung Thu KiDo’s Bakery",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2022/08/KIDOthuthinhvuong-xanh-scaled.jpg",
+            "name":"Hộp Bánh Trung Thu KIDO THU THỊNH VƯỢNG XANH – Hộp Quà Biếu Cao Cấp 2023",
+            "price":"450.000 ₫",
+            "type":"Bánh Trung Thu KiDo’s Bakery",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2022/08/KIDOthunhuy-2048x1883.jpg",
+            "name":"Hộp Bánh Trung Thu KIDO THU NHƯ Ý – Hộp Quà Biếu Cao Cấp 2023",
+            "price":"370.000 ₫",
+            "type":"Bánh Trung Thu KiDo’s Bakery",
+        },
+        {
+            "img":"	https://banhkinhdo.com.vn/wp-content/uploads/2023/07/KIDOthuannhien-2048x1883.jpg",
+            "name":"Hộp Bánh Trung Thu KIDO THU AN KHANG – Hộp Quà Biếu Cao Cấp 2023",
+            "price":"350.000 ₫",
+            "type":"Bánh Trung Thu KiDo’s Bakery",
+        },
+    ],
+    [
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2021/08/hop-2-banh-300x300.jpg",
+            "name":"Thu Bình An – Hộp 2 bánh nướng 210g ( 1 Mặn, 1 Ngọt)",
+            "price":"250.000 ₫",
+            "type":"Hộp Bánh Trung Thu 2 – 4 -6 Bánh 2023",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2021/08/hop-2-banh-300x300.jpg",
+            "name":"Thu An Khang – Hộp 2 bánh 150G (1 Mặn + 1 Ngọt)",
+            "price":"159.000 ₫",
+            "type":"Hộp Bánh Trung Thu 2 – 4 -6 Bánh 2023",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2021/08/hop-4-banh-300x300.jpg",
+            "name":"Thu Vạn Lộc – Hộp 4 Bánh 210G ( 2 Mặn, 2 Ngọt)",
+            "price":"499.000 ₫",
+            "type":"Hộp Bánh Trung Thu 2 – 4 -6 Bánh 2023",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2021/08/hop-4-banh-300x300.jpg",
+            "name":"Thu Trường Thọ – Hộp 4 Bánh Lớn Đặc Biệt ( 3 Nướng, 1 Dẻo)",
+            "price":"459.000 ₫",
+            "type":"Hộp Bánh Trung Thu 2 – 4 -6 Bánh 2023",
+        },
+        {
+            "img":"https://banhkinhdo.com.vn/wp-content/uploads/2021/08/hop-4-banh-300x300.jpg",
+            "name":"Thu Vạn Tài – Hộp 4 Bánh Ngọt Đặc Biệt",
+            "price":"429.000 ₫",
+            "type":"Hộp Bánh Trung Thu 2 – 4 -6 Bánh 2023",
+        },
+    ]
+]
+
+let container = document.getElementById('container')
+
+container.innerHTML=''
+for (let i of data){
+    let count = 0
+    let render = `<div class="container-list">`
+    for(let j of i){
+        if(count = 0){
+            render+=`<h3 class="content-cake">${j.type}</h3><div class="container-content">`
+            count++
+        }
+        render+=
+        `<div> class="content-items">
+            <img class="content-img"
+            src="${j.img}" alt="">
+            <p class="content-name">${j.name}</p>
+            <p class="content-price">${j.price}</p>
+            <button class="content-button">Đặt Hàng</button>
+        </div>`
+    }
+    render+=`</div></div>`   
+    console.log(render)
+    container.innerHTML+=render
+}
+
